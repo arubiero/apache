45544d4b0b3db5dec1f63f1f89cc862aa795a164
Adrian Rubiero <adrian@rubiero.com>
2017-06-12 19:39:11 +0200

    commit

--- recipes/apache.rb
+++ recipes/apache.rb
@@ -4,6 +4,10 @@ service 'httpd' do
   action [:enable, :start]
 end
 
-file '/var/www/html/index.html'do
-  content "<h1>Hello</h1>\n<h2>IPADDRESS: #{node["ipaddress"]}</h2><h2> HOSTNAME: #{node["hostname"]}</h2>"  
+template '/var/www/html/index.html' do
+  source 'index.html.erb'
 end
+
+#file '/var/www/html/index.html'do
+#  content "<h1>Hello</h1>\n<h2>IPADDRESS: #{node["ipaddress"]}</h2><h2> HOSTNAME: #{node["hostname"]}</h2>"  
+#end
